3361765a7c1f6be20a08b4f6ff0f553d1292072a
kristerevensen <67630525+kristerevensen@users.noreply.github.com>
2020-12-16 00:41:39 +0100

    changed from lumen to laravel

---
+++ app/Models/Click.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Click extends Model
+{
+    use HasFactory;
+
+    public function get_redirect($id)
+    {
+        return DB::table('campaigns_links')
+                ->where('link_token',$id)
+                ->pluck('tagged_url');
+
+    }
+}
