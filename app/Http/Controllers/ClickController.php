4475d6b947403ccf92d3f102aecf8c54f605aefc
kristerevensen <67630525+kristerevensen@users.noreply.github.com>
2020-12-16 19:07:28 +0100

    added fix to token problems

--- app/Http/Controllers/ClickController.php
+++ app/Http/Controllers/ClickController.php
@@ -42,7 +42,7 @@ class ClickController extends Controller
             $data['platform'] = "desktop";
         }
         $click = new Click();
-        if(Click::where('link_token',$id)->isEmpty()) {
+        if(!Click::where('link_token',$id)->get()) {
             redirect('https://marcam.tech',301)->send();
         }
         $click = new Click();
